5ed2a9cf20494edb8dcdb478da65fc5858c6d56c
Jessie Hanay <cesihanay@gmail.com>
2026-05-24 15:37:31 +0300

    Delete ConsoleApp2 directory

--- ConsoleApp2/IBookRepository.cs
+++
@@ -1,9 +0,0 @@
-//Interface for book repository:
-public interface IBookRepository
-{
-    bool Add(Book book);
-    Book? GetById(int id);
-    Book[] GetAll();
-    bool Update(Book book);
-    bool Delete(int id);
-}
